652817f549752bb9d7a179d3c3185d19d0aeca16
Dirley Rodrigues <dirleyrls@gmail.com>
2014-10-27 12:42:57 -0200

    Move source into src

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH="$(npm bin):$PATH"
 trap 'kill $(jobs -p)' EXIT
-jsx --watch ./components/src ./components &
+jsx --watch ./src/components/src ./src/components &
 exec node "$(which nwbuild)" -v 0.10.5 -r . &
 wait %2
